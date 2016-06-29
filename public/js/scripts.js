100ad3ba00411359756b13d59e45eae706b3dc3f
Patrick Lipscomb <patrick@>
2016-06-29 16:36:25 -0700

    with jquery

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
   $('#player_2_span').on('click', function() {
     $('#player_2').hide()
   });
+  $('#computer-play').on('click', function() {
+    selector = Math.floor(Math.random() * 4)
+    array = ['rock', 'paper', 'scissors']
+    value = array[selector]
+    $('#player_2').val(value)
+  })
 });
