733185763223f20952653048bc905a400a95e3e3
Patrick Lipscomb <Guest@epicodus-32.local>
2016-07-15 08:07:41 -0700

    resolve issues with spec testing.

--- spec/word_freq_integration_spec.rb
+++ spec/word_freq_integration_spec.rb
@@ -11,11 +11,4 @@ describe('the word frequency path', {:type => :feature}) do
     click_button('Click Here')
     expect(page).to have_content("'the' appears: 2 times.")
   end
-  it('processes the user string and returns the number of times that string appears') do
-    visit('/')
-    fill_in("input_string", :with => 'bacon bits of bacon')
-    fill_in('find', :with => 'bacon')
-    click_button('Click Here')
-    expect(page).to have_content("'bacon' appears: 2 times.")
-  end
 end
--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -2,13 +2,16 @@ require('rspec')
 require('word_freq')
 
 describe("String#word_freq") do
-  it("counts the number of occurrences of a string within a different string") do
-    expect(("bacon bits").word_freq("bacon")).to(eq(1))
+  it("returns if an inputted string that is test for returns") do
+    expect(("bits").word_freq("bits")).to(eq(1))
+  end
+  it("returns if looked for string is within another string") do
+    expect(("bit of some bits").word_freq('bit')).to(eq(1))
   end
   it("counts the number of occurrences of a string within a different string") do
     expect(("friday today is friday").word_freq("friday")).to(eq(2))
   end
-  it("counts the number of occurrences of a string within a different string") do
+  it("counts the number of occurrences of a string within a different string regardless of punctuation marks and capitalization") do
     expect(("That bat! It flew into the window to meet my baseball bat.").word_freq("BAT")).to(eq(2))
   end
 end
