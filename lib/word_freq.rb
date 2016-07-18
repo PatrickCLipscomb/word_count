b1373a2ffbdaec8b9b53903fa663bcb2fdba6abe
patricklipscomb <patricklipscomb@Patricks-MacBook-Pro.local>
2016-07-17 19:41:16 -0700

    make requested changes in variable naming changes and spec changes.

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -1,12 +1,12 @@
 class String
   define_method(:word_freq) do |find|
-    to_count = find.downcase
+    searched_str = find.downcase
     str = self.downcase
     str.delete! ",.!?'"
     str_array = str.split(' ')
     counter = 0
     str_array.each() do |word|
-      if word == to_count
+      if word == searched_str
         counter += 1
       end
     end
--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -2,13 +2,13 @@ require('rspec')
 require('word_freq')
 
 describe("String#word_freq") do
-  it("returns if an inputted string that is test for returns") do
+  it("determines if the searched for strin matches the inputted string") do
     expect(("bits").word_freq("bits")).to(eq(1))
   end
-  it("returns if looked for string is within another string") do
+  it("determines if the searched for string is within the inputted string") do
     expect(("bit of some bits").word_freq('bit')).to(eq(1))
   end
-  it("counts the number of occurrences of a string within a different string") do
+  it("counts the number of occurrences of a searched for string within an inputted string") do
     expect(("friday today is friday").word_freq("friday")).to(eq(2))
   end
   it("counts the number of occurrences of a string within a different string regardless of punctuation marks and capitalization") do
